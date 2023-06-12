59ed56ea05b30489c0d2c50a211505fc029dd737
acetelles <115594718+acetelles@users.noreply.github.com>
2023-06-12 17:33:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();    
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}  
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+
+    ellipse(12, 8, 0);
+    circle(40, 40, 40);
+}
